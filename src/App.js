80d771dc93eca033a7d0fb2798deb6561dd31f8e
het0814 <het08142003@gmail.com>
2025-03-09 23:59:39 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useRef } from 'react';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
@@ -6,34 +6,50 @@ import TechnicalSkills from './components/TechnicalSkills';
 import Contact from './components/Contact';
 import './App.css';
 
-function App() {
-  useEffect(() => {
-    const smoothScroll = (targetId) => {
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
-    };
+// function App() {
+//   return (
+//     <div>
+//       <Hero />
+//       <Experience />
+//       <TechnicalSkills />
+//       <Projects />
+//       <Contact />
+//     </div>
+//   );
+// }
 
-    const links = document.querySelectorAll('nav a');
-    links.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        smoothScroll(targetId);
-      });
-    });
-  }, []);
+// export default App;
+const App = () => {
 
+  const experienceRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div>
+      <nav className="navbar">
+        <ul>
+          <li><button onClick={() => scrollToSection(experienceRef)}>Experience</button></li>
+          <li><button onClick={() => scrollToSection(skillsRef)}>Skills</button></li>
+          <li><button onClick={() => scrollToSection(projectsRef)}>Projects</button></li>
+        </ul>
+      </nav>
       <Hero />
-      <Experience />
-      <TechnicalSkills />
-      <Projects />
+      <div ref={experienceRef}>
+        <Experience />
+      </div>
+      <div ref={skillsRef} className="section">
+        <TechnicalSkills />
+      </div>
+      <div ref={projectsRef} className="section">
+        <Projects />
+      </div>
       <Contact />
     </div>
   );
-}
+};
 
 export default App;
