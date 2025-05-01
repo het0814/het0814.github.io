bcec12e8a4598aeb9d9e11707c189333ceae0aef
het0814 <het08142003@gmail.com>
2025-05-01 15:10:14 -0400

    changed job title format

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,19 +5,19 @@ const Experience = () => {
     <section id="experience">
       <h2>Experience</h2>
       <ul>
-        <li><strong>Sheridan College, Centre for Applied AI – <span style={{ color: '#d16969' }}>Artificial Intelligence Resercher (Part-Time)</span></strong><br></br><span style={{ color: '#808080' }}>September 2024 – April 2025</span><br></br>
+        <li><strong><span style={{ color: '#d16969' }}>Artificial Intelligence Resercher (Part-Time)</span> - Sheridan College, Centre for Applied AI (CAAI)</strong><br></br><span style={{ color: '#808080' }}>September 2024 – April 2025</span><br></br>
         •Led research and development for AI/ML-based pipelines for fleet management.<br></br>
         •Worked with LLM (Large Language Models) to design a KPI scorecard system for fleet management based on industrial documents.<br></br>
         •Used BERTopic for advanced topic modeling and analysis of key insights from fleet-related reports.<br></br>
         •Conducted extensive research by reviewing academic papers, industry standards, and emerging tools in AI/ML.<br></br>
         •Worked on code, prompts, and workflows to test and validate approaches, ensuring project goals were met with quality results.<br></br>
         •Regularly communicated and presented project progress, findings, and outcomes to industry partners, ensuring alignment with business goals and collaboration.</li>
-        <li><strong>Naryant, Centre for Applied AI – <span style={{ color: '#d16969' }}>AI/ML Software Developer (CO-OP)</span></strong><br></br><span style={{ color: '#808080' }}>May 2024 – September 2024</span><br></br>
+        <li><strong><span style={{ color: '#d16969' }}>AI/ML Software Developer (CO-OP)</span> - Naryant, Centre for Applied AI (CAAI)</strong><br></br><span style={{ color: '#808080' }}>May 2024 – September 2024</span><br></br>
         •Built a CTGAN-based synthetic data generation pipeline for SUMO mobility simulations, enhancing dataset availability and realism for transportation studies.<br></br>
         •Designed, trained, and optimized machine learning models for transportation mode detection, achieving 92% improvement in classification accuracy.<br></br>
         •Collaborated closely with stakeholders to translate project requirements into actionable technical solutions, ensuring project alignment and success.<br></br>
         •Utilized Python (Pandas, Scikit-learn, TensorFlow), SUMO, and CTGAN (Generative AI Model) frameworks to deliver powerful and scalable models.</li>
-        <li><strong>MAGNA INTERNATIONAL, Karmax Heavy Stamping – <span style={{ color: '#d16969' }}>IIot Developer (CO-OP)</span></strong><br></br><span style={{ color: '#808080' }}>May 2023 – February 2024</span><br></br>
+        <li><strong><span style={{ color: '#d16969' }}>IIot Developer (CO-OP)</span> - MAGNA INTERNATIONAL</strong><br></br><span style={{ color: '#808080' }}>May 2023 – February 2024</span><br></br>
         •Created Web Pages in ignition software for maintenance department to monitor various machinery status and detect faults.<br></br>
         •Modified PLC programs to retrieve live data from PLC, integrating them seamlessly into different visualization components for efficient data analysis.<br></br>
         •Successfully reduced troubleshooting time by 85% and increased monitoring accuracy by 97% through strategic improvements in processes and technology.<br></br>
